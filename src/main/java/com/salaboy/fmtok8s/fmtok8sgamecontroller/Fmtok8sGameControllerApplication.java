dbf8276147534660415bc0f90debc565573d2ed8
salaboy <Salaboy@gmail.com>
2022-02-13 08:57:40 +0000

    initial commit

---
+++ src/main/java/com/salaboy/fmtok8s/fmtok8sgamecontroller/Fmtok8sGameControllerApplication.java
@@ -0,0 +1,13 @@
+package com.salaboy.fmtok8s.fmtok8sgamecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fmtok8sGameControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fmtok8sGameControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salaboy/fmtok8s/fmtok8sgamecontroller/Fmtok8sGameControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salaboy.fmtok8s.fmtok8sgamecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fmtok8sGameControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
